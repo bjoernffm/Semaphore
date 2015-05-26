038fcc7c178baa1631dd19b1070d6035c08f48eb
Björn Ebbrecht <bjoern.ebbrecht@gmail.com>
2015-05-27 00:48:04 +0200

    added function definitions

--- Semaphore.php
+++ Semaphore.php
@@ -2,20 +2,29 @@
 
 namespace Semaphore;
 
+use \Exception;
+
 class Semaphore
 {
     private $handle;
 
-    public function __construct()
-    {
-        if (is_writable($this->getFilename() . '.test'))
-            throw new \Exception('Lockfile "' . $this->getFilename() . '" is not writable');
-    }
-
+    /**
+     * Lock method
+     *
+     * This method is for locking the semaphore. If already locked, this method
+     * returns false. An optional way is to wait until the semaphore is being
+     * unlocked from another process.
+     * 
+     * @param  boolean $waitUntilUnlocked False by default. Waits until unlocked if true 
+     * @return boolean Returns true if locking was successful, else false
+     * @throws Exception If lock file could not be created
+     */
     public function lock($waitUntilUnlocked = false)
     {
-        $this->handle = fopen($this->getFilename(), "c+");
-        var_dump($waitUntilUnlocked);
+        $this->handle = @fopen($this->getFilename(), "c+");
+        if ($this->handle === false)
+            throw new Exception('Lockfile "' . $this->getFilename() . '" is not writable');
+
         $operation = LOCK_EX;
         
         if (!$waitUntilUnlocked)
@@ -27,25 +36,32 @@ class Semaphore
         return true;
     }
 
+    /**
+     * Unlock method
+     *
+     * This method unlocks the semaphore.
+     */
     public function unlock()
     {
-        flock($this->handle, LOCK_UN);
-        fclose($this->handle);
-        //unlink($this->getFilename());
+        @flock($this->handle, LOCK_UN);
+        @fclose($this->handle);
     }
 
+    /**
+     * Destructor method
+     */
     public function __destruct()
     {
-        //$this->unlock();
+        $this->unlock();
     }
 
     private function getFilename()
     {
         $fn = sha1($_SERVER['SCRIPT_NAME']) . '.lock';
         $subDir = substr($fn, 0, 2);
-        
+
         if (!is_dir(__DIR__ . '/locks/' . $subDir))
-            mkdir(__DIR__ . '/locks/' . $subDir);
+            mkdir(__DIR__ . '/locks/' . $subDir, 0777, true);
 
         return __DIR__ . '/locks/' . $subDir . '/' . $fn;
     }
--- test.php
+++ test.php
@@ -5,11 +5,16 @@ use \Semaphore\Semaphore;
 require_once 'Semaphore.php';
 
 $semaphore = new Semaphore();
-if ($semaphore->lock()) {
+
+if ($semaphore->lock(true)) {
+
     echo 'Doing things';
+
     fgetc(STDIN);
+    
     echo 'Ready';
     $semaphore->unlock();
+
 } else {
     echo 'could not start';
 }
